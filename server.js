3460efdb87a65f0f3ee3bb1087e3cdfce71c1820
StarRrScreaM <158058564+StarRrScreaM@users.noreply.github.com>
2026-01-09 20:45:40 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
+const io = require("socket.io")(3000, {
+  cors: { origin: "*" }
+})
+
+let users = []
+
+io.on("connection", socket => {
+
+  socket.on("join", name => {
+    users.push({ id: socket.id, name })
+    io.emit("users", users.map(u => u.name))
+  })
+
+  socket.on("message", msg => {
+    io.emit("message", msg)
+  })
+
+  socket.on("disconnect", () => {
+    users = users.filter(u => u.id !== socket.id)
+    io.emit("users", users.map(u => u.name))
+  })
+
+})
 
